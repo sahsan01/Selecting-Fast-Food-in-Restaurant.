1f4421db33fee3ef23c31ef886feb956951e93c8
sahsan01 <106379874+sahsan01@users.noreply.github.com>
2022-06-02 04:44:29 -0400

    Selecting Fast Food

---
+++ KFC.sh
@@ -0,0 +1,15 @@
+echo "WELCOME TO KFC"
+echo "HEllo Sir!"
+echo "What would you like to eat?"
+echo "1) Broast"
+echo "2) Zinger Burger"
+echo "3) Fries"
+echo "4) Saucy Wings"
+read case
+case $case in
+1) echo "You've selected Broast";;
+2) echo "You've selected Zinger Burger";;
+3) echo "You've selected Fries";;
+4) echo "You've selected Saucy Burger";;
+5) exit
+esac
